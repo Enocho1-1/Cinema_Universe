72fc89320371b540847a4ab57c3ff1567393504d
Enocho1-1 <enoch.okyere@gmail.com>
2023-09-16 15:38:36 -0400

    updates

--- src/pages/InitialPage.js
+++ src/pages/InitialPage.js
@@ -1,8 +1,19 @@
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router"
 import { InitialHeader } from "./Initial/InitialHeader"
 import arrow from "../assets/right-arrow.png"
 
 export const InitialPage = () => {
+
+  const navigate = useNavigate()
+
+  const initialSubmit = (event) => {
+    event.preventDefault()
+    const userEmail = event.target.email.value
+    sessionStorage.setItem("user", JSON.stringify(userEmail))
+    event.target.reset()
+    navigate("/register")
+  }
+
   return (
     <main className="initial h-screen w-screen">
         < InitialHeader/>
@@ -13,15 +24,15 @@ export const InitialPage = () => {
           <p className="text-white mt-4 font-semibold font-sans text-xl max-[812px]:text-lg">Ready to watch? Enter your email to create or restart your membership.</p>
           <>
 
-            <form className="mt-4 flex items-center max-[600px]:flex-col">   
+            <form onSubmit={initialSubmit} className="mt-4 flex items-center max-[600px]:flex-col">   
                 <div className="relative w-full">
-                    <input type="email" id="voice-search" className="initialInput border border-gray-300 text-white text-sm rounded-lg block pl-10 py-3 px-48" placeholder="Email Address" required/>
+                    <input type="email" name="email" id="voice-search" className="initialInput border border-gray-300 text-white text-sm rounded-lg block pl-10 py-3 px-48" placeholder="Email Address" required/>
                 </div>
                 <div className="w-full max-md:w-[50%] max-md:mt-4">
-                  <Link to={"/register"} type="submit" className="flex items-center px-6 py-2 ml-2 font-medium text-white bg-primary hover:bg-red-800 font-teko rounded-lg">
+                  <button  className="flex items-center px-6 py-2 ml-2 font-medium text-white bg-primary hover:bg-red-800 font-teko rounded-lg"type="submit">
                     <h1 className="inline text-xl">Get Started</h1>
                     <img src={arrow} className="h-6 ml-4" alt="" />
-                  </Link>
+                  </button>
                 </div>
              
             </form>
