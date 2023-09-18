cbd5c302656a4b12a0dc0f9955cfbeeaa54444ca
Enocho1-1 <73598581+Enocho1-1@users.noreply.github.com>
2023-09-17 21:14:34 -0400

    updates

--- src/components/elements/LoginDropDown.js
+++ src/components/elements/LoginDropDown.js
@@ -2,6 +2,8 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
+import Bookmark from "../../assets/bookmark-white.png"
+import Logout from "../../assets/logout.png"
 
 export const LoginDropDown = () => {
 
@@ -21,15 +23,18 @@ export const LoginDropDown = () => {
           <div className="font-sans font-medium truncate">{username}</div>
       </div>
       <ul className="py-1 font-sans text-sm dark:text-gray-200" aria-labelledby="dropdownUserAvatarButton">
-          <li>
-              <Link to="" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">WatchList</Link>
+          <li className="flex">
+              <Link to="" className="block py-2 px-4">WatchList</Link>
+
+              <img src={Bookmark} className="h-4 self-center" alt="" />
           </li>
           {/* <li>
               <Link to="" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</Link>
           </li> */}
       </ul>
-      <div className="py-1">
-          <span onClick={handleLogout} className="cursor-pointer block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log out</span>
+      <div className="py-1 flex">
+          <span onClick={handleLogout} className="cursor-pointer block py-2 px-4 text-sm text-gray-700 dark:text-gray-200 dark:hover:text-white">Log out</span>
+          <img src={Logout} className="ml-4 h-4 self-center" alt="" />
       </div>
   </div>
   )
