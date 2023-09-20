5a38f35df916c2363d16f5c2a56e38cc781639b1
Enocho1-1 <enoch.okyere@gmail.com>
2023-09-20 02:58:09 -0400

    responsive design

--- src/pages/Home/components/DefaultCarousel.js
+++ src/pages/Home/components/DefaultCarousel.js
@@ -10,32 +10,71 @@ export const DefaultCarousel = () => {
    
   return (
     <div className=" CarouselContain">
-        <Carousel className="m-0 p-0 ">
+        <Carousel>
             {/* Slide 1 */}
-            <figure className="relative w-full">
-                <img  className="w-full" src="https://www.slashfilm.com/img/gallery/al-pacino-was-almost-fired-two-weeks-into-filming-the-godfather/l-intro-1670452428.jpg" alt="..."/>
-                <figcaption className="carouselCaption font-sans flex flex-col">
-                    <h1 className="font-semibold text-gray-200 text-6xl max-lg:text-4xl ">Movie Title</h1>
+            <figure>
+                <img  className="mainImg" src="https://www.slashfilm.com/img/gallery/al-pacino-was-almost-fired-two-weeks-into-filming-the-godfather/l-intro-1670452428.jpg" alt="..."/>
+                <figcaption className="carouselCaption font-sans ">
+                    <h1 className="font-semibold text-gray-200 text-6xl max-lg:text-3xl ">Movie Title</h1>
+                    <aside className="text-md mt-4 max-w-6xl flex max-tablet:justify-center">
+                        <div className="font-semibold text-sm px-2 rounded-lg bg-primary-blue">HD</div>
+                        <div className="font-semibold text-sm ml-3 px-2 rounded-lg border border-gray-200 text-gray-200">PG-13</div>
+                        <>
+                            <img src={star} className="h-4 ml-2 self-center" alt="" />
+                            <p className="ml-2 text-sm text-gray-200">9.5/10</p>
+                        </>
+                        <p className="ml-2 text-sm text-gray-200">2023</p>
+                        <p className="ml-2 text-sm text-gray-200">114 min</p>
+                    </aside>
 
-                    
-                    <aside className="text-md mt-4 max-w-6xl flex ">
+                    {/* Genres  */}
+                    <aside className=" flex flex-wrap mt-4 px-2 max-w-3xl max-tablet:justify-center">
+                            <p className="text-lg mx-2 text-gray-200">comedy</p>
+                            <p className="text-lg mx-2 text-gray-200">romance</p>
+                            <p className=" text-lg mx-2 text-gray-200">action</p>
+                    </aside>
+                    {/* Genres */}
+                    <aside className="max-w-4xl mt-4 ">
+                        <p className="text-gray-200 max-[990px]:hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, amet. Ullam nisi totam tenetur rerum a maiores in illo blanditiis unde tempora omnis quisquam soluta repellendus provident quidem alias, id deserunt suscipit sunt voluptatibus veritatis quod aut asperiores quia. Voluptatem totam est commodi, enim magni ea. Quibusdam, itaque explicabo. Deleniti!</p>
+                    </aside> 
+
+                    {/* Buttons */}
+                    <aside className="mt-2 max-w-2xl flex max-tablet:justify-center">
+                        <button className="bg-primary-blue rounded-xl p-2 flex items-center"> 
+                            <img src={play} className="h-6" alt="" />
+                            <p className="font-semibold text-md ml-2">Watch Now</p> 
+                        </button>
+                        <button className="ml-3 rounded-xl p-2 flex items-center max-tablet:justify-center "> 
+                            <img src={bookmark} className="h-6" alt="" />
+                            <p className="font-semibold text-md ml-2 text-gray-200">Bookmark</p> 
+                        </button>
+                    </aside>
+
+                </figcaption>
+            </figure>
+          
+            <figure className=" relative  w-full">
+                <img  className="mainImg" src="https://raisingworldchildren.com/wp-content/uploads/2018/11/the-pursuit-of-happyness.jpg" alt="..."/>
+                <figcaption className="carouselCaption font-sans    ">
+                    <h1 className="font-semibold text-gray-200 text-6xl max-lg:text-3xl  ">Movie Title</h1>
+                    {/* <aside className="text-md mt-4 max-w-4xl flex ">
                         <span className="font-semibold text-sm px-2 rounded-lg bg-primary-blue">HD</span>
                         <span className="font-semibold text-sm ml-3 px-2 rounded-lg border border-gray-200 text-gray-200">PG-13</span>
-                        <span className=" max-w-xl ml-2 flex items-center">
-                            <img src={star} className="h-[5px] w-2" alt="" />
+                        <span className=" ml-2 flex items-center">
+                            <img src={star} className="h-4" alt="" />
                             <p className="ml-2 text-sm text-gray-200">9.5</p>
                         </span>
-                        {/* <span className=" ml-2 flex">
+                        <span className=" ml-2 flex">
                             <p className="ml-2 text-sm text-gray-200">2023</p>
                             <p className="ml-2 text-sm text-gray-200">114 min</p>
                             <p className="ml-2 text-sm text-gray-200">comedy</p>
                             <p className="ml-2 text-sm text-gray-200">romance</p>
                             <p className="ml-2 text-sm text-gray-200">action</p>
-                        </span> */}
+                        </span>
                     </aside>
-                    <aside className="max-w-4xl mt-4 ">
+                    <aside className="max-w-4xl mt-4">
                         <p className="text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, amet. Ullam nisi totam tenetur rerum a maiores in illo blanditiis unde tempora omnis quisquam soluta repellendus provident quidem alias, id deserunt suscipit sunt voluptatibus veritatis quod aut asperiores quia. Voluptatem totam est commodi, enim magni ea. Quibusdam, itaque explicabo. Deleniti!</p>
-                    </aside>
+                    </aside> */}
 
                     {/* Buttons */}
                     {/* <aside className="mt-2 max-w-2xl flex">
@@ -52,49 +91,11 @@ export const DefaultCarousel = () => {
                 </figcaption>
             </figure>
           
-            <figure className=" relative  w-full">
-                <img  className="w-full" src="https://raisingworldchildren.com/wp-content/uploads/2018/11/the-pursuit-of-happyness.jpg" alt="..."/>
-                <figcaption className="carouselCaption font-sans    ">
-                    <h1 className="font-semibold text-gray-200 text-6xl max-lg:text-4xl ">Movie Title</h1>
-                    <aside className="text-md mt-4 max-w-4xl flex ">
-                        <span className="font-semibold text-sm px-2 rounded-lg bg-primary-blue">HD</span>
-                        <span className="font-semibold text-sm ml-3 px-2 rounded-lg border border-gray-200 text-gray-200">PG-13</span>
-                        <span className=" ml-2 flex items-center">
-                            <img src={star} className="h-4" alt="" />
-                            <p className="ml-2 text-sm text-gray-200">9.5</p>
-                        </span>
-                        <span className=" ml-2 flex">
-                            <p className="ml-2 text-sm text-gray-200">2023</p>
-                            <p className="ml-2 text-sm text-gray-200">114 min</p>
-                            <p className="ml-2 text-sm text-gray-200">comedy</p>
-                            <p className="ml-2 text-sm text-gray-200">romance</p>
-                            <p className="ml-2 text-sm text-gray-200">action</p>
-                        </span>
-                    </aside>
-                    <aside className="max-w-4xl mt-4">
-                        <p className="text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, amet. Ullam nisi totam tenetur rerum a maiores in illo blanditiis unde tempora omnis quisquam soluta repellendus provident quidem alias, id deserunt suscipit sunt voluptatibus veritatis quod aut asperiores quia. Voluptatem totam est commodi, enim magni ea. Quibusdam, itaque explicabo. Deleniti!</p>
-                    </aside>
-
-                    {/* Buttons */}
-                    <aside className="mt-2 max-w-2xl flex">
-                        <button className="bg-primary-blue rounded-xl p-2 flex items-center"> 
-                            <img src={play} className="h-6" alt="" />
-                            <p className="font-semibold text-md ml-2">Watch Now</p> 
-                        </button>
-                        <button className="ml-3 rounded-xl p-2 flex items-center"> 
-                            <img src={bookmark} className="h-6" alt="" />
-                            <p className="font-semibold text-md ml-2 text-gray-200">Bookmark</p> 
-                        </button>
-                    </aside>
-
-                </figcaption>
-            </figure>
-          
             <figure className=" relative w-full">
-                <img  className="w-full" src="https://m.media-amazon.com/images/M/MV5BMTgyMDUzMDYxNl5BMl5BanBnXkFtZTcwMzAzNDQyNw@@._V1_.jpg" alt="..."/>
+                <img  className="mainImg" src="https://m.media-amazon.com/images/M/MV5BMTgyMDUzMDYxNl5BMl5BanBnXkFtZTcwMzAzNDQyNw@@._V1_.jpg" alt="..."/>
                 <figcaption className="carouselCaption font-sans   ">
-                    <h1 className="font-semibold text-gray-200 text-6xl max-lg:text-4xl ">Movie Title</h1>
-                    <aside className="text-md mt-4 max-w-4xl flex ">
+                    <h1 className="font-semibold text-gray-200 text-6xl max-lg:text-3xl ">Movie Title</h1>
+                    {/* <aside className="text-md mt-4 max-w-4xl flex ">
                         <span className="font-semibold text-sm px-2 rounded-lg bg-primary-blue">HD</span>
                         <span className="font-semibold text-sm ml-3 px-2 rounded-lg border border-gray-200 text-gray-200">PG-13</span>
                         <span className=" ml-2 flex items-center">
@@ -111,10 +112,10 @@ export const DefaultCarousel = () => {
                     </aside>
                     <aside className="max-w-4xl mt-4">
                         <p className="text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, amet. Ullam nisi totam tenetur rerum a maiores in illo blanditiis unde tempora omnis quisquam soluta repellendus provident quidem alias, id deserunt suscipit sunt voluptatibus veritatis quod aut asperiores quia. Voluptatem totam est commodi, enim magni ea. Quibusdam, itaque explicabo. Deleniti!</p>
-                    </aside>
+                    </aside> */}
 
                     {/* Buttons */}
-                    <aside className="mt-2 max-w-2xl flex">
+                    {/* <aside className="mt-2 max-w-2xl flex">
                         <button className="bg-primary-blue rounded-xl p-2 flex items-center"> 
                             <img src={play} className="h-6" alt="" />
                             <p className="font-semibold text-md ml-2">Watch Now</p> 
@@ -123,16 +124,16 @@ export const DefaultCarousel = () => {
                             <img src={bookmark} className="h-6" alt="" />
                             <p className="font-semibold text-md ml-2 text-gray-200">Bookmark</p> 
                         </button>
-                    </aside>
+                    </aside> */}
 
                 </figcaption>
             </figure>
         
             <figure className=" relative  w-full">
-                <img  className="w-full" src="https://a.ltrbxd.com/resized/sm/upload/ho/gl/t1/5p/ex%20machina-1200-1200-675-675-crop-000000.jpg?v=ae03cd4b51" alt="..."/>
+                <img  className="mainImg" src="https://a.ltrbxd.com/resized/sm/upload/ho/gl/t1/5p/ex%20machina-1200-1200-675-675-crop-000000.jpg?v=ae03cd4b51" alt="..."/>
                 <figcaption className="carouselCaption font-sans   ">
-                    <h1 className="font-semibold text-gray-200 text-6xl max-lg:text-4xl ">Movie Title</h1>
-                    <aside className="text-md mt-4 max-w-4xl flex ">
+                    <h1 className="font-semibold text-gray-200 text-6xl max-lg:text-3xl ">Movie Title</h1>
+                    {/* <aside className="text-md mt-4 max-w-4xl flex ">
                         <span className="font-semibold text-sm px-2 rounded-lg bg-primary-blue">HD</span>
                         <span className="font-semibold text-sm ml-3 px-2 rounded-lg border border-gray-200 text-gray-200">PG-13</span>
                         <span className=" ml-2 flex items-center">
@@ -149,10 +150,10 @@ export const DefaultCarousel = () => {
                     </aside>
                     <aside className="max-w-4xl mt-4">
                         <p className="text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, amet. Ullam nisi totam tenetur rerum a maiores in illo blanditiis unde tempora omnis quisquam soluta repellendus provident quidem alias, id deserunt suscipit sunt voluptatibus veritatis quod aut asperiores quia. Voluptatem totam est commodi, enim magni ea. Quibusdam, itaque explicabo. Deleniti!</p>
-                    </aside>
+                    </aside> */}
 
                     {/* Buttons */}
-                    <aside className="mt-2 max-w-2xl flex">
+                    {/* <aside className="mt-2 max-w-2xl flex">
                         <button className="bg-primary-blue rounded-xl p-2 flex items-center"> 
                             <img src={play} className="h-6" alt="" />
                             <p className="font-semibold text-md ml-2">Watch Now</p> 
@@ -161,16 +162,16 @@ export const DefaultCarousel = () => {
                             <img src={bookmark} className="h-6" alt="" />
                             <p className="font-semibold text-md ml-2 text-gray-200">Bookmark</p> 
                         </button>
-                    </aside>
+                    </aside> */}
 
                 </figcaption>
             </figure>
       
             <figure className=" relative  w-full">
-                <img  className="w-full" src="https://m.media-amazon.com/images/S/pv-target-images/86dfd3549f93ed320fa617005385b59dbcb9fa801fc78cf4752c479a0a1e936c.jpg" alt="..."/>
+                <img  className="mainImg" src="https://m.media-amazon.com/images/S/pv-target-images/86dfd3549f93ed320fa617005385b59dbcb9fa801fc78cf4752c479a0a1e936c.jpg" alt="..."/>
                 <figcaption className="carouselCaption font-sans   ">
-                    <h1 className="font-semibold text-gray-200 text-6xl max-lg:text-4xl ">Movie Title</h1>
-                    <aside className="text-md mt-4 max-w-4xl flex ">
+                    <h1 className="font-semibold text-gray-200 text-6xl max-lg:text-3xl ">Movie Title</h1>
+                    {/* <aside className="text-md mt-4 max-w-4xl flex ">
                         <span className="font-semibold text-sm px-2 rounded-lg bg-primary-blue">HD</span>
                         <span className="font-semibold text-sm ml-3 px-2 rounded-lg border border-gray-200 text-gray-200">PG-13</span>
                         <span className=" ml-2 flex items-center">
@@ -187,10 +188,10 @@ export const DefaultCarousel = () => {
                     </aside>
                     <aside className="max-w-4xl mt-4">
                         <p className="text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, amet. Ullam nisi totam tenetur rerum a maiores in illo blanditiis unde tempora omnis quisquam soluta repellendus provident quidem alias, id deserunt suscipit sunt voluptatibus veritatis quod aut asperiores quia. Voluptatem totam est commodi, enim magni ea. Quibusdam, itaque explicabo. Deleniti!</p>
-                    </aside>
+                    </aside> */}
 
                     {/* Buttons */}
-                    <aside className="mt-2 max-w-2xl flex">
+                    {/* <aside className="mt-2 max-w-2xl flex">
                         <button className="bg-primary-blue rounded-xl p-2 flex items-center"> 
                             <img src={play} className="h-6" alt="" />
                             <p className="font-semibold text-md ml-2">Watch Now</p> 
@@ -199,7 +200,7 @@ export const DefaultCarousel = () => {
                             <img src={bookmark} className="h-6" alt="" />
                             <p className="font-semibold text-md ml-2 text-gray-200">Bookmark</p> 
                         </button>
-                    </aside>
+                    </aside> */}
 
                 </figcaption>
             </figure>
