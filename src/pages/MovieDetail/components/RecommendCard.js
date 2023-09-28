681a0b518239b9fb558f16081932ed44116aff4f
Enocho1-1 <enoch.okyere@gmail.com>
2023-09-28 10:34:37 -0400

    updates

--- src/pages/MovieDetail/components/RecommendCard.js
+++ src/pages/MovieDetail/components/RecommendCard.js
@@ -13,9 +13,9 @@ export const RecommendCard = ({item}) => {
     <>
             <Link to={`/${id}`}  className="flex max-w-[300px] max-lg:flex-col rounded-lg mt-2 mx-2 bg-gray-900 hover:cursor-pointer hover:bg-gray-600">
                 <img src={poster_path ? posterImage : collage} className="max-h-[150px] min-w-[120px] max-lg:h-[250px] max-lg:w-fit" alt="" />
-                <aside className="mt-1 ml-2 py-4 flex flex-col">
+                <aside className="mt-1 ml-2 text-ellipsis overflow-hidden ... max-mobileLg:py-4 flex flex-col">
                     <p className="font-sans text-md text-gray-500">{media_type}/ { release_date ? release_date : first_air_date} </p>
-                    <h1 className="mt-2 font-sans text-lg text-gray-200 text-ellipsis overflow-hidden">{title ? title : name}</h1>
+                    <h1 className="mt-2 font-sans text-lg text-gray-200  ">{title ? title : name}</h1>
                 </aside>
             </Link>
     </>
