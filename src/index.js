a6dca1b2186030a8bbfbac40730c424ce07e471a
Enocho1-1 <enoch.okyere@gmail.com>
2023-10-05 01:39:41 -0400

    npm i react-query

--- src/index.js
+++ src/index.js
@@ -3,30 +3,36 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { ScrollTop } from './components/index';
+import { QueryClient,QueryClientProvider } from 'react-query';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 import App from './App';
 
 
+const client = new QueryClient()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <ToastContainer
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={true}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable={false}
-      pauseOnHover
-      theme="dark"
-    />
-      <ScrollTop/>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+        theme="dark"
+      />
+        <ScrollTop/>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+ 
 
   </React.StrictMode>
 );
