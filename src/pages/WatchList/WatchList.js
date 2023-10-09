9dd54a4c61cfc2316448fc7207b80f95b4fef1fb
Enocho1-1 <enoch.okyere@gmail.com>
2023-10-09 11:38:33 -0400

    filter buttons

--- src/pages/WatchList/WatchList.js
+++ src/pages/WatchList/WatchList.js
@@ -18,7 +18,12 @@ export const WatchList = ({title}) => {
             <h1 className="font-bold text-4xl text-gray-300 flex justify-start ml-6 max-tablet:justify-center"><img src={play} className="h-8 self-center mr-2" alt="logo"/>{title}</h1>
             <button onClick={() => dispatch({type:"CLEAR"})} className="absolute right-[8%] p-2 text-gray-200 rounded-lg font-sans font-semibold bg-primary-blue hover:bg-secondary-blue">Clear All</button>
           </header>
-
+            {/* Filter Buttons */}
+            <aside className="watchFilter max-w-lg m-auto my-6 flex justify-center">
+              <span className=" font-sans font-semibold text-gray-300 text-xl hover:cursor-pointer hover:text-primary-blue mx-4">ALL</span>
+              <span className="font-sans font-semibold text-gray-300 text-xl hover:cursor-pointer hover:text-primary-blue mx-4">MOVIES</span>
+              <span className="font-sans font-semibold text-gray-300 text-xl hover:cursor-pointer hover:text-primary-blue mx-4">TV</span>
+            </aside>
             <aside className="mt-8 px-4 grid grid-cols-fiveCols max-mobile:grid-cols-twoCols mobile:max-mobileLg:grid-cols-threeCols mobileLg:max-tablet:grid-cols-fourCols gap-y-6 place-content-center">
               { list.map((item,index) => (
                 <WatchCard key={index} item={item} />
