b597b797e8c494a4fd1fd3deb80d704088298e00
Enocho1-1 <enoch.okyere@gmail.com>
2023-10-27 11:35:29 -0400

    initial page responsive design

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -67,29 +67,29 @@ export const Register = () => {
     <main className="relative initial h-screen w-screen">
       <InitialSecondHead/>
 
-      <div className="inputFieldContain max-h-[600px] w-[500px] max-md:w-[350px] min-xl:mt-[120px]">
-        <span className="text-white font-sans mt-6 px-4">
+      <div className="inputFieldContain max-h-[600px] w-[500px] max-md:w-[350px] min-2xl:mt-[120px]">
+        <span className="text-white font-sans min-xl:mt-6 px-4">
           <h1 className="font-semibold text-2xl">Create a password to start your membership</h1>
           <p className="text-lg mt-4">One step closer to entering the Cinema Universe 🚀</p>
         </span>
         
-        <form onSubmit={handleRegister} className="mt-6 px-3 py-2">
-        <div className="mb-6">
-          <label htmlFor="name" className="block mb-2 text-lg font-teko font-medium text-white">Name</label>
-          <input type="name" id="name" name="name" className=" bg-slate-700 border border-gray-300 text-white text-sm rounded-lg  block w-full p-2.5" placeholder="Enter Name" required/>
-          </div> 
+        <form onSubmit={handleRegister} className="min-xl:mt-6px-3 py-2">
           <div className="mb-6">
-          <label htmlFor="email" className="block mb-2 text-lg font-teko font-medium text-white">Email address</label>
-          <input onChange={e => setUEmail(e.target.value)} type="email" id="email" name="email" className=" bg-slate-700 border border-gray-300 text-white text-sm rounded-lg  block w-full p-2.5" value={ userName  ? userName  : email} required/>
-          </div> 
-          <div className="mb-6">
-              <label htmlFor="password" className="block mb-2 text-lg font-teko font-medium text-white">Password</label>
-              <input type="password" id="password" name="password" className="bg-slate-700 border border-gray-300 text-white text-sm rounded-lg  block w-full p-2.5" placeholder="•••••••••" required/>
-          </div> 
+            <label htmlFor="name" className="block mb-2 text-lg font-teko font-medium text-white">Name</label>
+            <input type="name" id="name" name="name" className=" bg-slate-700 border border-gray-300 text-white text-sm rounded-lg  block w-full p-2.5" placeholder="Enter Name" required/>
+            </div> 
+            <div className="mb-6">
+            <label htmlFor="email" className="block mb-2 text-lg font-teko font-medium text-white">Email address</label>
+            <input onChange={e => setUEmail(e.target.value)} type="email" id="email" name="email" className=" bg-slate-700 border border-gray-300 text-white text-sm rounded-lg  block w-full p-2.5" value={ userName  ? userName  : email} required/>
+            </div> 
+            <div className="mb-6">
+                <label htmlFor="password" className="block mb-2 text-lg font-teko font-medium text-white">Password</label>
+                <input type="password" id="password" name="password" className="bg-slate-700 border border-gray-300 text-white text-sm rounded-lg  block w-full p-2.5" placeholder="•••••••••" required/>
+            </div> 
 
-          <button  className="flex justify-center px-6 py-2 ml-2 font-medium text-white  bg-primary-blue hover:bg-secondary-blue font-teko rounded-lg w-full"type="submit">
-            <h1 className="inline text-xl">Register</h1>
-          </button>
+            <button  className="flex justify-center px-6 py-2 ml-2 font-medium text-white  bg-primary-blue hover:bg-secondary-blue font-teko rounded-lg w-full"type="submit">
+              <h1 className="inline text-xl">Register</h1>
+            </button>
         </form>
       </div>
 
