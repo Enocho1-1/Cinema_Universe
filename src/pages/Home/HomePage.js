0eb5885e16b782c6a1ffbdc04bb2e609a2ecff06
Enocho1-1 <enoch.okyere@gmail.com>
2023-10-29 20:25:56 -0400

    home page edit

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -54,13 +54,13 @@ export const HomePage = () => {
         < DefaultCarousel/>
 
         {/* Trending Movies Section */}
-        <section className="mt-6 m-auto max-w-[1750px]">
+        <section className="mt-6 m-auto max-2xl:max-w-[1500px] 2xl:max-w-[1750px]">
           <h1 className="font-sans font-semibold text-xl text-gray-300 text-center"><i className="bi bi-fire"></i>Trending Now<i className="bi bi-fire"></i></h1>
           <Trending />
         </section>
 
         {/* Upcoming Movies Section */}
-        <section className="relative mt-[175px]  px-6">
+        <section className="relative mt-[75px]  px-6">
           <h1 className="font-bold text-4xl text-gray-300 flex justify-start ml-6 max-tablet:justify-center"><img src={play} className="h-8 self-center mr-2"/>UPCOMING</h1>
           <UpComing/>
         </section>
