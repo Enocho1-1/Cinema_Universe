1446adebb20baacce3c78b4ae8f063ad0738a529
Enocho1-1 <enoch.okyere@gmail.com>
2023-11-25 04:07:07 -0500

    movie list page number update

--- src/pages/MovieList/MovieList.js
+++ src/pages/MovieList/MovieList.js
@@ -30,7 +30,7 @@ export const MovieList = ({apiPath, title, type}) => {
       { myQuery && !myQuery.matches ? <Header/> : <MobileHeader/>}
       <section className=" max-w-7xl m-auto mt-[100px]">
         <header className="movieList p-4">
-          <h1 className="font-bold text-4xl text-gray-300 flex justify-start ml-6 max-tablet:justify-center"><img src={play} className="h-8 self-center mr-2"/>{title}</h1>
+          <h1 className="font-bold text-4xl text-gray-300 flex justify-start ml-6 max-tablet:justify-center"><img src={play} className="h-8 self-center mr-2"/>{title} · {page}</h1>
         </header>
 
         {/* Movie/TV List */}
