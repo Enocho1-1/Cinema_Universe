376f4144cc029bdddc24ac4ed0b0cf68e7ea37ed
Enocho1-1 <enoch.okyere@gmail.com>
2024-01-25 14:14:17 -0500

    Update MovieCard component to use BlurEffect while image is loading

--- src/components/elements/MovieCard.js
+++ src/components/elements/MovieCard.js
@@ -13,27 +13,28 @@ export const MovieCard = ({item, type=""}) => {
     const itemInWatchList = state.list.find(movie => movie.id === item.id)
     const {imgloaded} = useBlurImg(posterImage)
   return (
-    <span className="MovieCard relative max-w-[200px] mx-4">
-        { itemInWatchList && (<img className="absolute top-2 right-2 h-4 w-4" src={bookmark_blue} alt="" />)}
-        <Link to={`/${id}`} onClick={() => saveMovie_TV_type(`${type ? type : media_type}`)} className="h-[250px] w-[170px] rounded-xl">
-            {!imgloaded?
-                (
-                <BlurEffect css="h-[250px] w-[170px] rounded-xl"/>
-                ):
-                (<img className="rounded-xl"  src={posterImage} loading="lazy" alt="movie-poster" />)
-            }
-          
-        </Link>
-        <aside className="mt-2 flex flex-col">
-            <div className="flex max-w-auto">
-                { type ? (<p className=" px-2 text-sm rounded-xl border border-gray-300  text-gray-300">{type}</p>) : (<p className=" px-2 text-sm rounded-xl border border-gray-300  text-gray-300">{media_type}</p>)}
-             
-                <p className="ml-3 text-md text-gray-300">{release_date ? release_date : first_air_date}</p>
-            </div>
-            <div className="mt-2">
-                    <h1 className="max-w-[150px] ml-2 text-md text-gray-300 hover:cursor-pointer truncate overflow-hidden ..." title={title ? title : name}>{title ? title : name}</h1>
-            </div>
-        </aside>
-    </span>
+    <>
+        {!imgloaded ? (<BlurEffect css="h-[300px] w-[200px] rounded-xl"/>): 
+        (   <span className="MovieCard relative max-w-[200px] mx-4">
+                { itemInWatchList && (<img className="absolute top-2 right-2 h-4 w-4" src={bookmark_blue} alt="" />)}
+                <Link to={`/${id}`} onClick={() => saveMovie_TV_type(`${type ? type : media_type}`)} className="h-[250px] w-[170px] rounded-xl">
+                    <img className="rounded-xl"  src={posterImage} loading="lazy" alt="movie-poster" />
+                </Link>
+                <aside className="mt-2 flex flex-col">
+                    <div className="flex max-w-auto">
+                        { type ? (<p className=" px-2 text-sm rounded-xl border border-gray-300  text-gray-300">{type}</p>) : (<p className=" px-2 text-sm rounded-xl border border-gray-300  text-gray-300">{media_type}</p>)}
+                    
+                        <p className="ml-3 text-md text-gray-300">{release_date ? release_date : first_air_date}</p>
+                    </div>
+                    <div className="mt-2">
+                            <h1 className="max-w-[150px] ml-2 text-md text-gray-300 hover:cursor-pointer truncate overflow-hidden ..." title={title ? title : name}>{title ? title : name}</h1>
+                    </div>
+                </aside>
+            </span>
+        )
+        }
+        
+    </>
+   
   )
 }
